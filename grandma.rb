be4b41de528f7a4566592c89d57e4178f61c8402
Leah Boyd <leahboyd08@gmail.com>
2019-03-14 15:53:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   
   
-  if phrase == "hello, grandma!".downcase
+  if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "hello, grandma!".upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
